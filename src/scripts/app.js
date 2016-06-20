c5953f4c1586626a860693e25ca9a2d83eb69bc1
John Laine <john.laine5896@gmail.com>
2016-06-20 15:19:58 +0000

    Map and markers are working

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,15 +3,48 @@
  */
 'use strict';
 
+var locations = [
+  {
+  title: 'one', 
+  position: {lat: 33.4246989, lng: -111.694253}
+  },
+  {
+  title: 'two', 
+  position: {lat: 33.425541, lng: -111.696109}
+  },
+  {
+  title: 'three', 
+  position: {lat: 33.424804, lng: -111.696090}
+  },
+  {
+  title: 'four', 
+  position: {lat: 33.424594, lng: -111.697066}
+  },
+  {
+  title: 'five', 
+  position: {lat: 33.424831, lng: -111.698879}
+  }
+  ];
 
-var map;
+
 
 function initMap() {
+  var mapCenterStart = {lat: 33.4246989, lng: -111.694253};
+  
   // Create a map object and specify the DOM element for display.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: mapCenterStart,
     scrollwheel: false,
-    zoom: 8
+    zoom: 15
+  });
+  
+  // Create a marker and set its position.
+  locations.forEach(function(loc) {
+    new google.maps.Marker({
+    map: map,
+    position: loc.position,
+    title: loc.title
+  });  
   });
 }
 
