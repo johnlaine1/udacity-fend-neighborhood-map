a4c59e4d373585b63421aa0d017f0c4f721fcae8
John Laine <john.laine5896@gmail.com>
2016-06-20 00:21:09 +0000

    Get the map on the page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,24 @@
-//
+/**
+ * Neighborhood Map
+ */
 'use strict';
+
+
+var map;
+
+function initMap() {
+  // Create a map object and specify the DOM element for display.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    zoom: 8
+  });
+}
+
+window.addEventListener('load', initMap);
+
+
+
+
+
+
